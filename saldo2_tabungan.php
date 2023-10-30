15df6591c3f84dfaec938bf9537e111c5465e020
yayangIzza <149374422+yayangIzza@users.noreply.github.com>
2023-10-30 19:35:26 +0700

    Add files via upload

---
+++ saldo2_tabungan.php
@@ -0,0 +1,21 @@
+<?php
+
+echo "Nama  : Yayang Izzatul Haq<br>";
+echo "NIM   : 22205052<br>";
+echo "Kelas : PBO<br>";
+
+
+echo "saldo awal anda sebesar Rp.$2500000 <br>";
+echo "Besar bunga anda $8[bunga]% <br>";
+echo "saldo target Rp.$6000000 <br>";
+
+echo "Jumlah bulan 1 Rp.$378000 bulan<br>";
+echo "Jumlah bulan 2 Rp.$4082400 bulan<br>";
+echo "Jumlah bulan 3 Rp.$4408992 bulan<br>";
+echo "Jumlah bulan 4 Rp.$4761711 bulan<br>";
+echo "Jumlah bulan 5 Rp.$5142647 bulan<br>";
+echo "Jumlah bulan 6 Rp.$5554058 bulan<br>";
+echo "Jumlah bulan 7 Rp.$5998382 bulan<br>";
+echo "Jumlah bulan 8 Rp.$6478252 bulan<br>";
+
+     ?>
